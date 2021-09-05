867efcf5cd22a65a9869926edd6b9819c5d48968
williamollio <williamollio98@gmail.com>
2021-09-05 20:40:12 +0200

    telephone number

--- telephone_number_validor.js
+++ telephone_number_validor.js
@@ -1,14 +1,26 @@
 function telephoneCheck(str) {
+    const nbr_par = /\(|\)/g;
+    const btw_par = /\(([^)]+)\)/g;
+    var regExp = /\(([^)]+)\)/;
+    let matches = str.match(regExp);
+    const nbr_dash = /\-/g;
+    const qst_mark = /\?/g;
 
-    let one = 0
-    if (str[0] == 1)
-        one = 1
-    if ((str.replace(/[^0-9]/g, "").length == 11) && one)
-         return true
-    // else if (str.replace(/[^0-9]/g, "").length == 10)
-    //     return true
-    // else
-    //     return false
+    if (((str.match(nbr_par) || []).length) % 2 != 0)
+        return false
+    else if (matches && matches[1].length > 3)
+        return false
+    else if ((str.match(nbr_dash) || []).length > 2)
+        return false
+    else if (str.match(qst_mark))
+        return false
+    else if (str[0] == 1
+        && (str.replace(/[^0-9]/g, "").length == 11))
+        return true
+    else if (str.replace(/[^0-9]/g, "").length == 10)
+        return true
+    else
+        return false
 }
 
-telephoneCheck("555-555-5555");
+telephoneCheck("1 456 789 4444")
