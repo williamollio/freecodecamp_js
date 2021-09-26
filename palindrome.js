eefeb315396fb1efd44ce1eec01da34c502f07cd
William <williamollio98@gmail.com>
2021-09-26 13:04:34 +0200

    add comments

--- palindrome.js
+++ palindrome.js
@@ -5,8 +5,12 @@ function palindrome(str) {
     let i;
     let flag = 0;
 
+    /* Delete spaces and convert all the uppercases to lowercases */
     str1 = str.replace(/[^0-9a-z]/gi, '').toLowerCase()
     length = str1.length;
+
+    /* Look through the string starting from the beginning and the end.
+    ** In order to check if the characters are the same */
     for (i = 0; i < length; i++) {
         if (str1[i] !== str1[length - 1])
             flag = 1;
