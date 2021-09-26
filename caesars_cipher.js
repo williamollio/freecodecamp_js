a9ae87006f4e7f8636ba09290d5f703d92288f77
William <williamollio98@gmail.com>
2021-09-26 12:51:53 +0200

    add comments

--- caesars_cipher.js
+++ caesars_cipher.js
@@ -1,13 +1,18 @@
-function rot13(str) {
+function rot13(str){
 
     let arr = []
     let arr1 = []
     let arr2 = []
+
+    /* Create an array of letters*/
     arr = Array.from(str)
+
     let code
     let i = 0
     let str1
 
+    /* Apply the substitution cipher ROT13 on each letter by modifying their ASCII Code.
+    ** And build a new array with their new ASCII Code*/
     arr.forEach(element => {
         if (element.charCodeAt() >= 65 && element.charCodeAt() <= 77) {
             code = element.charCodeAt() + 13
@@ -17,14 +22,18 @@ function rot13(str) {
             code = element.charCodeAt() - 13
             arr1[i] = code
         }
+        /* Push the spaces on the array without modification */
         else
             arr1.push(element.charCodeAt())
         i++;
     })
 
+    /* Build an array from ASCII Code to characters */
     arr1.forEach(element => arr2.push(String.fromCharCode(element)))
+
+    /* Recreate the string to return */
     str1 = arr2.join('')
-    return str1
+   return str1
 }
 
 rot13("SERR PBQR PNZC");
