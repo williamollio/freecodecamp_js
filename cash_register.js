c3d6d3721894d2224c228b6d03e7e61a8111a12a
William <williamollio98@gmail.com>
2021-09-26 11:07:08 +0200

    cash_register done

--- cash_register.js
+++ cash_register.js
@@ -12,6 +12,7 @@ function checkCashRegister(price, cash, cid) {
         ["NICKEL", 0.05],
         ["PENNY",0.01]
     ]
+
     /* Object to return */
     let object = {
         status : "",
@@ -29,43 +30,56 @@ function checkCashRegister(price, cash, cid) {
     /* Sum of the cid array */
     let sum = temp_sum[1].toFixed(2)
 
-    let given = 0;
+    let i = 0
     let times = 0;
-
-    console.log("sum : " + sum)
-    console.log("change : " + change)
-
-
+    let given = 0;
+    let sum_given = 0
+    let temp_arr = [...unit]
     cid.reverse()
-    console.log(cid)
-    let i
-    for (i = 0; i < unit.length; i++)
+
+    for (i; i < unit.length; i++)
     {
-        if (cid[i][0] != 0 && unit[i][1] < change)
-        {
             times = cid[i][1] / unit[i][1];
             while (times-- && unit[i][1] <= change)
             {
                 given += unit[i][1];
                 change -= unit[i][1];
+                change = change.toFixed(2)
             }
-            if (change == 0)
-                break;
-        }
+            if (given > 0)
+            {
+                temp_arr[i][1] = given
+                sum_given += given
+                given = 0
+            }
+            else if (given == 0)
+                temp_arr[i][1] = 0
     }
-    object.status = "OPEN"
-    object.change = cid[i]
-    console.log(object)
-    // if (change_original == sum) {
-    //     object.status = "CLOSED"
-    //     object.change = cid
-    //     return object
-    // }
 
-    // else if (change_original > sum) {
-    //     object.status = "INSUFFICIENT_FUNDS"
-    //     return object
-    // }
+    if (change_original == sum){
+        object.status = "CLOSED"
+        object.change = cid.reverse()
+        return object
+    }
+
+    else if (change_original > sum || change_original > sum_given){
+        object.status = "INSUFFICIENT_FUNDS"
+        return object
+    }
+
+    else{
+        let final_arr = []
+        for(let i = 0; i < temp_arr.length; i++)
+        {
+            if (temp_arr[i][1] != 0)
+                final_arr.push(temp_arr[i])
+        }
+        object.status = "OPEN"
+        object.change = final_arr
+        return object
+    }
 }
-// should return {status: "OPEN", change: [["QUARTER", 0.5]]}
-checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
+
+checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1],
+["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20],
+["TWENTY", 60], ["ONE HUNDRED", 100]])
